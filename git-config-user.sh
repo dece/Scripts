ebf747e2145f208b52c09c8060098984530d57db
Adrien Abraham <aabraham@freebox.fr>
2022-09-07 12:19:18 +0200

    git-config-user: show current config

--- git-config-user.sh
+++ git-config-user.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Quickly setup user name and email.
+echo "Current name: $(git config user.name)"
+echo "Current email: $(git config user.email)"
 read -p "User name: " name
 git config user.name "$name"
 read -p "User email: " email
