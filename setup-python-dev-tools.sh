8c021c1e15df98a86f09102daa00ae2d1550147b
Adrien Abraham <aabraham@freebox.fr>
2022-09-30 19:04:37 +0200

    update Python dev tools management

--- setup-python-dev-tools.sh
+++ setup-python-dev-tools.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-pip3 install python-lsp-server
-
 ask_install() {
     packages="$@"
     read -p "Install $packages? [y/n] " -n 1 -r ; echo
     [[ "$REPLY" = y ]] && pip3 install $packages
 }
 
-ask_install pyflakes
-ask_install black python-lsp-black
-ask_install mypy pylsp-mypy
+ask_install jedi-language-server
+ask_install pycodestyle
+ask_install pylint
+ask_install mypy
